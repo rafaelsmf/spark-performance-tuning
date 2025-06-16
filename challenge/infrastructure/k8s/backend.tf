aaa37b6707e83f39f13f38b35ce6069cdea09b30
Rafael Sousa <rafael.sousamf@gmail.com>
2025-06-16 00:04:46 -0300

    feat(iac): update required providers

--- challenge/infrastructure/k8s/backend.tf
+++ challenge/infrastructure/k8s/backend.tf
@@ -1,8 +1,13 @@
 terraform {
   required_providers {
     kubectl = {
-      source  = "gavinbunney/kubectl"
-      version = ">= 1.13.1"
+      source = "gavinbunney/kubectl"
+      version = "1.19.0"
+    }
+
+    helm = {
+      source = "hashicorp/helm"
+      version = "3.0.0-pre2"
     }
   }
 }
